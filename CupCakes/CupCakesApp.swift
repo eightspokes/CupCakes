a87d1166a58c507df5c77c79c018bb47a88a70e3
romanmuni8 <romanmuni8@gmail.com>
2022-12-21 10:07:33 -0500

    Initial Commit

---
+++ CupCakes/CupCakesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupCakesApp.swift
+//  CupCakes
+//
+//  Created by Roman on 12/21/22.
+//
+
+import SwiftUI
+
+@main
+struct CupCakesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
